4b640ff14e946b9fde478361d40718c47900c3d0
1842047134@qq.com <91363385+mlabalabala@users.noreply.github.com>
2024-03-02 12:26:43 +0800

    Update GenMM.py

--- GenMM.py
+++ GenMM.py
@@ -1,27 +1,46 @@
-import requests, re, urllib3
+import requests, urllib3
 
+urllib3.util.ssl_.DEFAULT_CIPHERS = "TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-128-GCM-SHA256:TLS13-AES-256-GCM-SHA384:ECDHE:!COMPLEMENTOFDEFAULT"
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+h = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A"}
+session = requests.Session()
+# 设置重试次数、间隔时间
+retry = Retry(total=3, backoff_factor=1,connect=8)
+adapter = HTTPAdapter(max_retries=retry)
+session.mount('http://', adapter)
+session.mount('https://', adapter)
+
 if __name__ == '__main__':
-    s = requests.session()
 
-    h = {
-        "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
-    }
+    # def_urls = ["dh44u","2cdas","rgcqe","pupbx","e8jt5"]
+    def_urls_res_url = "https://www.bunnyabc.eu.org:15245/d/mm/4hu_url_sub_string.json?sign=oWYvIN_lsML77vTp7Y24Ts3mbLg-KG5D-Mslneo0uV4=:0"
+    res = session.get(url=def_urls_res_url,headers=h,verify=False)
+    def_urls = json.loads(res.text)
+    print("def_urls:", def_urls)
+    redirect_urls = set()
+    for def_url in def_urls:
+        try:
+            res = session.get(url=f"https://www.{def_url}.com/",headers=h,verify=False,timeout=3)
+            redirect_urls.add(res.url)
+        except:
+            continue
+    json_str = json.dumps(list(redirect_urls))
+    print("json_str:", json_str)
+    with open("4hu_home_urls","w+",encoding="UTF-8") as w:
+        w.write(json_str)
 
+    '''
+    import re
     mmu = 'http://www.ef323.com'
-
     r = r'https://www\.\w+\.com'
-
-    mmstr = "{\"ua\":\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36\",\"homeUrl\":\"https://www.d75612f62232.com/index/home.html\",\"cateNode\":\"//*[@id='section-menu']/div/div[2]/ul/li[position()<8]/a[1]\",\"cateName\":\"/text()\",\"cateNameR\":\"(\\\\S+)\",\"cateId\":\"/@href\",\"cateIdR\":\"/shipin/list-(\\\\S+)-1.html\",\"homeVodNode\":\"//*[@id='section-menu']/div/div[2]/ul/li[position()<8]/a[1]\",\"homeVodName\":\"/text()\",\"homeVodNameR\":\"(\\\\S+)\",\"homeVodId\":\"/@href\",\"homeVodIdR\":\"(\\\\S+)\",\"homeVodImg\":\"/@href\",\"homeVodMark\":\"\",\"cateUrl\":\"https://www.d75612f62232.com/shipin/list-{cateId}-{catePg}.html\",\"cateVodNode\":\"//body\",\"cateVodName\":\"//a[contains(text(), '>') and not(contains(text(), '>>'))]/@href\",\"cateVodNameR\":\"/shipin/list-(\\\\S+).html\",\"cateVodId\":\"//a[contains(text(), '>') and not(contains(text(), '>>'))]/@href\",\"cateVodIdR\":\"(\\\\S+)\",\"cateVodImg\":\"//a[contains(text(), '>') and not(contains(text(), '>>'))]/@href\",\"cateVodMark\":\"\",\"dtUrl\":\"https://www.d75612f62232.com{vid}\",\"dtNode\":\"//body\",\"dtName\":\"//*[@id='section-menu']/div/div[2]/div/a/text()\",\"dtImg\":\"//*[@id='section-menu']/div/div[2]/div/a/text()\",\"dtCate\":\"//*[@id='section-menu']/div/div[2]/div/a/text()\",\"dtYear\":\"//*[@id='section-menu']/div/div[2]/div/a/text()\",\"dtArea\":\"//*[@id='section-menu']/div/div[2]/div/a/text()\",\"dtMark\":\"//*[@id='section-menu']/div/div[2]/div/a/text()\",\"dtActor\":\"//*[@id='section-menu']/div/div[2]/div/a/text()\",\"dtDirector\":\"//*[@id='section-menu']/div/div[2]/div/a/text()\",\"dtDesc\":\"//*[@id='section-menu']/div/div[2]/div/a/text()\",\"dtFromNode\":\"//*[@id='section-menu']/div/div[2]/div/a\",\"dtFromName\":\"/text()\",\"dtUrlNode\":\"//*[@id='main-container']/div[2]/div[2]/ul[1]\",\"dtUrlSubNode\":\"/li/a[1]\",\"dtUrlId\":\"/@href\",\"dtUrlIdR\":\"(\\\\S+)\",\"dtUrlName\":\"/@title\",\"dtUrlNameR\":\"(\\\\S+)\",\"playUrl\":\"https://www.d75612f62232.com{playUrl}\",\"playUa\":\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36\"}"
-
-    mmres = s.get(mmu, headers=h, verify=False)
+    mmres = session.get(mmu, headers=h, verify=False)
     mmurl = re.findall(r, mmres.url)[0]
-    mmstrmod = re.sub(r, mmurl, mmstr, 0, re.MULTILINE)
 
     with open('mmzxsp', 'w', encoding='utf8') as w:
         w.write(mmstrmod)
+    '''
+    
     '''
     with open('sex5offline', 'r', encoding='utf8') as r:
         s0 = r.read()
